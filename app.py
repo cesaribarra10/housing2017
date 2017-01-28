327fcb850702ebc21fff29daf5055bf460d766bc
Cesar Ibarra <cfi2103@columbia.edu>
2017-01-28 06:00:49 -0500

    added enviro var

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, flash, request
 from pymongo import MongoClient
-# import requests as r
-
+import os
 
 app = Flask(__name__)
 
@@ -30,5 +29,6 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     app.debug = True
