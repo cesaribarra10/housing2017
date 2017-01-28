2c4505a29221fe83cbe7df85691f5fba82f05011
Cesar Ibarra <cfi2103@columbia.edu>
2017-01-28 05:27:47 -0500

    posts to db, added procfile

--- app.py
+++ app.py
@@ -1,18 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
 from pymongo import MongoClient
+# import requests as r
+
 
 app = Flask(__name__)
+
+app.secret_key = 'some_secret'
 
 # change cesar:cesar to chan:chan @chan
 client = MongoClient('mongodb://cesar:cesar@ds155097.mlab.com:55097/housing')
 db = client.housing
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def home():
-    # db.names.insert({'name': 'cesar'})
-    return render_template('index.html')
+    # db.names.insert({'name': 'chan'})
+    if request.method == 'POST':
+        # get parameters from post
+        name = request.form["name"]
+        choice1 = request.form["choice1"]
+        choice2 = request.form["choice2"]
+        choice3 = request.form["choice3"]
+        db.names.insert({'name': name, 'list': [{'choice1': choice1, 'choice2': choice2, 'choice3': choice3}]})
+        flash('Thank you for submitting :D', 'success')
+        return render_template('index.html')
+    else:
+        print('stuff')
+        return render_template('index.html')
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
+    app.debug = True
